baa7e7aace1c2885ae0917ef1909e557876586f4
jaylaye <yejayla69@gmail.com>
2024-02-14 09:31:23 -0500

    .

--- vue-project/src/array.js
+++ vue-project/src/array.js
@@ -1,10 +1,762 @@
 const skins = [
+    [
+        //side arms
+
+    //frenzys
+            {
+              "name": "Origin",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "Nitro",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "Magepunk",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "BlastX",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "Champions 2023",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "Battle Pass Season 1 Act 3",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "Glitchpop",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "RGX 2.0",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "Horizon",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "Sakura",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "Prelude to Chaos",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "Gaia",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "Elderflame",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "Sovereign",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "RGX 11z Pro",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            },
+            {
+              "name": "Reaver",
+              "gun": "Frenzy",
+              "tier": "Standard"
+            }
+         
+            //shorty
+
+            [
+                {
+                  "name": "Protocol",
+                  "gun": "Shorty",
+                  "tier": "Standard"
+                },
+                {
+                  "name": "BlastX",
+                  "gun": "Shorty",
+                  "tier": "Standard"
+                },
+                {
+                  "name": "Champions 2023",
+                  "gun": "Shorty",
+                  "tier": "Standard"
+                },
+                {
+                  "name": "Sakura",
+                  "gun": "Shorty",
+                  "tier": "Standard"
+                },
+                {
+                  "name": "Prelude to Chaos",
+                  "gun": "Shorty",
+                  "tier": "Standard"
+                },
+                {
+                  "name": "Sovereign",
+                  "gun": "Shorty",
+                  "tier": "Deluxe"
+                },
+                {
+                  "name": "RGX 11z Pro",
+                  "gun": "Shorty",
+                  "tier": "Select"
+                },
+                {
+                  "name": "Oni",
+                  "gun": "Shorty",
+                  "tier": "Deluxe"
+                },
+                {
+                  "name": "Reaver",
+                  "gun": "Shorty",
+                  "tier": "Exclusive"
+                },
+                {
+                  "name": "Elderflame",
+                  "gun": "Shorty",
+                  "tier": "Deluxe"
+                },
+                {
+                  "name": "Luxe",
+                  "gun": "Shorty",
+                  "tier": "Select"
+                },
+                {
+                  "name": "Glitchpop",
+                  "gun": "Shorty",
+                  "tier": "Select"
+                },
+                {
+                  "name": "Duality",
+                  "gun": "Shorty",
+                  "tier": "Deluxe"
+                }
+              ]
+              
+        // Sheriff
+        {
+          "name": "Prime",
+          "gun": "Sheriff",
+          "tier": "Deluxe"
+        },
+        {
+          "name": "Reaver",
+          "gun": "Sheriff",
+          "tier": "Exclusive"
+        },
+        {
+          "name": "Glitchpop",
+          "gun": "Sheriff",
+          "tier": "Select"
+        },
+        {
+          "name": "Duality",
+          "gun": "Sheriff",
+          "tier": "Deluxe"
+        },
+        {
+          "name": "Sovereign",
+          "gun": "Sheriff",
+          "tier": "Deluxe"
+        },
+        {
+          "name": "RGX 11z Pro",
+          "gun": "Sheriff",
+          "tier": "Select"
+        },
+        {
+          "name": "Oni",
+          "gun": "Sheriff",
+          "tier": "Deluxe"
+        },
+        {
+          "name": "Protocol",
+          "gun": "Sheriff",
+          "tier": "Standard"
+        },
+        {
+          "name": "BlastX",
+          "gun": "Sheriff",
+          "tier": "Standard"
+        },
+        {
+          "name": "Champions 2023",
+          "gun": "Sheriff",
+          "tier": "Standard"
+        },
+        {
+          "name": "Prelude to Chaos",
+          "gun": "Sheriff",
+          "tier": "Standard"
+        },
+        {
+          "name": "Sakura",
+          "gun": "Sheriff",
+          "tier": "Standard"
+        },
+        {
+          "name": "Elderflame",
+          "gun": "Sheriff",
+          "tier": "Deluxe"
+        },
+        {
+          "name": "Luxe",
+          "gun": "Sheriff",
+          "tier": "Select"
+        },
+        {
+          "name": "Gaia",
+          "gun": "Sheriff",
+          "tier": "Standard"
+        },
+        {
+          "name": "Nitro",
+          "gun": "Sheriff",
+          "tier": "Standard"
+        },
+        {
+          "name": "Magepunk",
+          "gun": "Sheriff",
+          "tier": "Standard"
+        },
+        {
+          "name": "RGX 2.0",
+          "gun": "Sheriff",
+          "tier": "Select"
+        },
+        {
+          "name": "Glitchpop 2.0",
+          "gun": "Sheriff",
+          "tier": "Select"
+        },
+        {
+          "name": "Horizon",
+          "gun": "Sheriff",
+          "tier": "Standard"
+        },
+      
+        // Ghost
+        {
+          "name": "Sakura",
+          "gun": "Ghost",
+          "tier": "Standard"
+        },
+        {
+          "name": "Prelude to Chaos",
+          "gun": "Ghost",
+          "tier": "Standard"
+        },
+        {
+          "name": "Sovereign",
+          "gun": "Ghost",
+          "tier": "Deluxe"
+        },
+        {
+          "name": "Reaver",
+          "gun": "Ghost",
+          "tier": "Exclusive"
+        },
+        {
+          "name": "RGX 11z Pro",
+          "gun": "Ghost",
+          "tier": "Select"
+        },
+        {
+          "name": "Oni",
+          "gun": "Ghost",
+          "tier": "Deluxe"
+        },
+        {
+          "name": "Protocol",
+          "gun": "Ghost",
+          "tier": "Standard"
+        },
+        {
+          "name": "BlastX",
+          "gun": "Ghost",
+          "tier": "Standard"
+        },
+        {
+          "name": "Champions 2023",
+          "gun": "Ghost",
+          "tier": "Standard"
+        },
+        {
+          "name": "Elderflame",
+          "gun": "Ghost",
+          "tier": "Deluxe"
+        },
+        {
+          "name": "Luxe",
+          "gun": "Ghost"
+
+
+
+
+
+
+
+
+        //SMGS
+
+            //stinger
+
+          {
+            "name": "RGX 11z Pro",
+            "gun": "Stinger",
+            "tier": "Select"
+          },
+          {
+            "name": "Glitchpop",
+            "gun": "Stinger",
+            "tier": "Select"
+          },
+          {
+            "name": "Reaver",
+            "gun": "Stinger",
+            "tier": "Exclusive"
+          },
+          {
+            "name": "Sovereign",
+            "gun": "Stinger",
+            "tier": "Deluxe"
+          },
+          {
+            "name": "RGX 2.0",
+            "gun": "Stinger",
+            "tier": "Select"
+          },
+          {
+            "name": "Horizon",
+            "gun": "Stinger",
+            "tier": "Standard"
+          },
+          {
+            "name": "Radiant Guardian",
+            "gun": "Stinger",
+            "tier": "Battle Pass"
+          },
+          {
+            "name": "Champions 2023",
+            "gun": "Stinger",
+            "tier": "Standard"
+          },
+          {
+            "name": "BlastX",
+            "gun": "Stinger",
+            "tier": "Standard"
+          },
+          {
+            "name": "Luxe",
+            "gun": "Stinger",
+            "tier": "Select"
+          },
+          {
+            "name": "Nitro",
+            "gun": "Stinger",
+            "tier": "Standard"
+          },
+          {
+            "name": "Magepunk",
+            "gun": "Stinger",
+            "tier": "Standard"
+          },
+        
+          // Spectre
+          {
+            "name": "Reaver",
+            "gun": "Spectre",
+            "tier": "Exclusive"
+          },
+          {
+            "name": "Glitchpop",
+            "gun": "Spectre",
+            "tier": "Select"
+          },
+          {
+            "name": "RGX 11z Pro",
+            "gun": "Spectre",
+            "tier": "Select"
+          },
+          {
+            "name": "Sovereign",
+            "gun": "Spectre",
+            "tier": "Deluxe"
+          },
+          {
+            "name": "RGX 2.0",
+            "gun": "Spectre",
+            "tier": "Select"
+          },
+          {
+            "name": "Horizon",
+            "gun": "Spectre",
+            "tier": "Standard"
+          },
+          {
+            "name": "Nitro",
+            "gun": "Spectre",
+            "tier": "Standard"
+          },
+          {
+            "name": "Magepunk",
+            "gun": "Spectre",
+            "tier": "Standard"
+          },
+          {
+            "name": "BlastX",
+            "gun": "Spectre",
+            "tier": "Standard"
+          },
+          {
+            "name": "Luxe",
+            "gun": "Spectre",
+            "tier": "Select"
+          }
+
+
+          
+
+            // Judge
+  {
+    "name": "Elderflame",
+    "gun": "Judge",
+    "tier": "Deluxe"
+  },
+  {
+    "name": "Glitchpop",
+    "gun": "Judge",
+    "tier": "Select"
+  },
+  {
+    "name": "Reaver",
+    "gun": "Judge",
+    "tier": "Exclusive"
+  },
+  {
+    "name": "Sovereign",
+    "gun": "Judge",
+    "tier": "Deluxe"
+  },
+  {
+    "name": "RGX 11z Pro",
+    "gun": "Judge",
+    "tier": "Select"
+  },
+  {
+    "name": "RGX 2.0",
+    "gun": "Judge",
+    "tier": "Select"
+  },
+  {
+    "name": "Horizon",
+    "gun": "Judge",
+    "tier": "Standard"
+  },
+  {
+    "name": "BlastX",
+    "gun": "Judge",
+    "tier": "Standard"
+  },
+  {
+    "name": "Nitro",
+    "gun": "Judge",
+    "tier": "Standard"
+  },
+  {
+    "name": "Magepunk",
+    "gun": "Judge",
+    "tier": "Standard"
+  },
+  {
+    "name": "Luxe",
+    "gun": "Judge",
+    "tier": "Select"
+  },
+
+  // Bucky
+  {
+    "name": "Reaver",
+    "gun": "Bucky",
+    "tier": "Exclusive"
+  },
+  {
+    "name": "Sovereign",
+    "gun": "Bucky",
+    "tier": "Deluxe"
+  },
+  {
+    "name": "RGX 11z Pro",
+    "gun": "Bucky",
+    "tier": "Select"
+  },
+  {
+    "name": "RGX 2.0",
+    "gun": "Bucky",
+    "tier": "Select"
+  },
+  {
+    "name": "BlastX",
+    "gun": "Bucky",
+    "tier": "Standard"
+  },
+  {
+    "name": "Nitro",
+    "gun": "Bucky",
+    "tier": "Standard"
+  },
+  {
+    "name": "Magepunk",
+    "gun": "Bucky",
+    "tier": "Standard"
+  },
+  {
+    "name": "Champions 2023",
+    "gun": "Bucky",
+    "tier": "Standard"
+  },
+  {
+    "name": "Luxe",
+    "gun": "Bucky",
+    "tier": "Select"
+  }
+
+    // Vandal
     {
-      //  skinName:
-      //  category:
-      //  price:
+        "name": "Elderflame",
+        "gun": "Vandal",
+        "tier": "Deluxe"
+      },
+      {
+        "name": "Glitchpop",
+        "gun": "Vandal",
+        "tier": "Select"
+      },
+      {
+        "name": "Reaver",
+        "gun": "Vandal",
+        "tier": "Exclusive"
+      },
+      {
+        "name": "Sovereign",
+        "gun": "Vandal",
+        "tier": "Deluxe"
+      },
+      {
+        "name": "RGX 11z Pro",
+        "gun": "Vandal",
+        "tier": "Select"
+      },
+      {
+        "name": "RGX 2.0",
+        "gun": "Vandal",
+        "tier": "Select"
+      },
+      {
+        "name": "Horizon",
+        "gun": "Vandal",
+        "tier": "Standard"
+      },
+      {
+        "name": "BlastX",
+        "gun": "Vandal",
+        "tier": "Standard"
+      },
+      {
+        "name": "Nitro",
+        "gun": "Vandal",
+        "tier": "Standard"
+      },
+      {
+        "name": "Magepunk",
+        "gun": "Vandal",
+        "tier": "Standard"
+      },
+      {
+        "name": "Luxe",
+        "gun": "Vandal",
+        "tier": "Select"
+      },
+    
+      // Phantom
+      {
+        "name": "Elderflame",
+        "gun": "Phantom",
+        "tier": "Deluxe"
+      },
+      {
+        "name": "Glitchpop",
+        "gun": "Phantom",
+        "tier": "Select"
+      },
+      {
+        "name": "Reaver",
+        "gun": "Phantom",
+        "tier": "Exclusive"
+      },
+      {
+        "name": "Sovereign",
+        "gun": "Phantom",
+        "tier": "Deluxe"
+      },
+      {
+        "name": "RGX 11z Pro",
+        "gun": "Phantom",
+        "tier": "Select"
+      },
+      {
+        "name": "RGX 2.0",
+        "gun": "Phantom",
+        "tier": "Select"
+      },
+      {
+        "name": "Horizon",
+        "gun": "Phantom",
+        "tier": "Standard"
+      },
+      {
+        "name": "BlastX",
+        "gun": "Phantom",
+        "tier": "Standard"
+      },
+      {
+        "name": "Nitro",
+        "gun": "Phantom",
+        "tier": "Standard"
+      },
+      {
+        "name": "Magepunk",
+        "gun": "Phantom",
+        "tier": "Standard"
+      },
+      {
+        "name": "Luxe",
+        "gun": "Phantom",
+        "tier": "Select"
+      }
 
- }
 
+
+
+
+      //rifles
+
+
+      //operator
+      [
+        // Operator
+        {
+          "name": "Protocol",
+          "gun": "Operator",
+          "tier": "Standard"
+        },
+        {
+          "name": "BlastX",
+          "gun": "Operator",
+          "tier": "Standard"
+        },
+        {
+          "name": "Champions 2023",
+          "gun": "Operator",
+          "tier": "Standard"
+        },
+        {
+          "name": "Sakura",
+          "gun": "Operator",
+          "tier": "Standard"
+        },
+        {
+          "name": "Prelude to Chaos",
+          "gun": "Operator",
+          "tier": "Standard"
+        },
+        {
+          "name": "Sovereign",
+          "gun": "Operator",
+          "tier": "Deluxe"
+        },
+        {
+          "name": "RGX 11z Pro",
+          "gun": "Operator",
+          "tier": "Select"
+        },
+        {
+          "name": "Oni",
+          "gun": "Operator",
+          "tier": "Deluxe"
+        },
+        {
+          "name": "Reaver",
+          "gun": "Operator",
+          "tier": "Exclusive"
+        },
+        {
+          "name": "Elderflame",
+          "gun": "Operator",
+          "tier": "Deluxe"
+        },
+        {
+          "name": "Luxe",
+          "gun": "Operator",
+          "tier": "Select"
+        },
+        {
+          "name": "Glitchpop",
+          "gun": "Operator",
+          "tier": "Select"
+        },
+        {
+          "name": "Duality",
+          "gun": "Operator",
+          "tier": "Deluxe"
+        },
+        {
+          "name": "Horizon",
+          "gun": "Operator",
+          "tier": "Standard"
+        },
+        {
+          "name": "Magepunk",
+          "gun": "Operator",
+          "tier": "Standard"
+        },
+        {
+          "name": "RGX 2.0",
+          "gun": "Operator",
+          "tier": "Select"
+        },
+        {
+          "name": "Nitro",
+          "gun": "Operator",
+          "tier": "Standard"
+        },
+        {
+          "name": "Radiant Guardian",
+          "gun": "Operator",
+          "tier": "Battle Pass"
+        }
+      ]
+      
 ]
 export {skins};
